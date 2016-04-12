c19b6f98feec54b906d3a7767d5002c661a5bb2b
estarter <estarter@gmail.com>
2016-04-12 18:15:41 +0200

    add screen fixture

--- test/screen_test.cc
+++ test/screen_test.cc
@@ -1,9 +1,21 @@
 #include "gtest/gtest.h"
 #include "screen.h"
 
-TEST(Screen, changing) {
-    Screen screen(10);
-    screen.clearScreen();
+
+class ScreenTest : public ::testing::Test {
+protected:
+    Screen screen;
+    ScreenTest() : screen(10) {}
+
+    virtual void SetUp() {
+        screen.clearScreen();
+    }
+
+  // virtual void TearDown() {}
+};
+
+
+TEST_F(ScreenTest, changing) {
     ASSERT_EQ(' ', screen[0]);
     screen[0] = 'x';
     ASSERT_EQ('x', screen[0]);
@@ -11,23 +23,21 @@ TEST(Screen, changing) {
     ASSERT_EQ(' ', screen[0]);
 }
 
-TEST(Screen, copy_ctor) {
-    Screen orig_screen(10);
-    orig_screen.clearScreen();
-    orig_screen[0] = 'x';
+TEST_F(ScreenTest, copy_ctor) {
+    screen[0] = 'x';
 
-    Screen copy_screen = orig_screen;
+    Screen copy_screen = screen;
     ASSERT_EQ('x', copy_screen[0]);
     copy_screen[0] = 'o';
     ASSERT_EQ('o', copy_screen[0]);
-    ASSERT_EQ('x', orig_screen[0]);
+    ASSERT_EQ('x', screen[0]);
 
     copy_screen.clearScreen();
     ASSERT_EQ(' ', copy_screen[0]);
-    ASSERT_EQ('x', orig_screen[0]);
+    ASSERT_EQ('x', screen[0]);
 
     copy_screen[0] = 'o';
-    orig_screen.clearScreen();
+    screen.clearScreen();
     ASSERT_EQ('o', copy_screen[0]);
-    ASSERT_EQ(' ', orig_screen[0]);
+    ASSERT_EQ(' ', screen[0]);
 }
