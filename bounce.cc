feea854b1d730dc917312992b1e4b4fb0e311264
estarter <estarter@gmail.com>
2016-04-13 09:33:28 +0200

    check input file

--- bounce.cc
+++ bounce.cc
@@ -6,37 +6,41 @@
 
 
 int main() {
-  Screen screen(maxColumn+1);
-  Particle* particleList;
+    Screen screen(maxColumn+1);
+    Particle* particleList;
 
-  int particleAmount;
+    int particleAmount;
 
-  std::string filename = "bounce.cfg";
-  std::ifstream in(filename);
-  if (!in) {
-    std::cerr << "Could not open file: " << filename << std::endl;
-    return EXIT_FAILURE;
-  } else {
-    in >> particleAmount;
-    particleList = new Particle[particleAmount];
-    for (int i = 0; i < particleAmount; i++) {
-      char sym;
-      double pos, speed;
-      in >> sym >> pos >> speed;
-      particleList[i].set(sym, pos, speed);
+    std::string filename = "bounce.cfg";
+    std::ifstream in(filename);
+    if (!in) {
+        std::cerr << "Could not open file: " << filename << std::endl;
+        return EXIT_FAILURE;
+    } else {
+        in >> particleAmount;
+        particleList = new Particle[particleAmount];
+        for (int i = 0; i < particleAmount; i++) {
+            char sym;
+            double pos, speed;
+            in >> sym >> pos >> speed;
+            if (!in.good()) {
+                std::cerr << "Bad data in file: " << filename << " , line number: " << i+2 << std::endl;
+                return EXIT_FAILURE;
+            }
+            particleList[i].set(sym, pos, speed);
+        }
     }
-  }
 
-  int timeStep = 0;
-  int stopTime = 60;
+    int timeStep = 0;
+    int stopTime = 60;
 
-  while (timeStep < stopTime) {
-    screen.clearScreen();
-    for (int i = 0; i < particleAmount; i++) {
-      particleList[i].drawParticle(screen);
-      particleList[i].moveParticle();
+    while (timeStep < stopTime) {
+        screen.clearScreen();
+        for (int i = 0; i < particleAmount; i++) {
+            particleList[i].drawParticle(screen);
+            particleList[i].moveParticle();
+        }
+        screen.drawScreen();
+        timeStep++;
     }
-    screen.drawScreen();
-    timeStep++;
-  }
 }
